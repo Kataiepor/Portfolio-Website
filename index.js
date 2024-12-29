9644a9c10274d2d38308104a243e86fca8f3d48e
Daniel Khataiepour <118835404+Kataiepor@users.noreply.github.com>
2024-12-28 19:07:22 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var pomodoroVideo = document.getElementById("Videos/Pomodoro.mp4");
+    pomodoroVideo.play();
+});
+
+/*================================= scroll section active link ===================================*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    /*================================= sticky navbar ===================================*/
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*================================= remove toggle icon and navbar when click navbar link (mobile) ===================================*/
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
+
+/*================================= typed js ===================================*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Future Electrical Engineer', 'Programmer', 'Developer', 'Sketcher/Painter'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+});
